6d7ce84a0e3093d52fc7f908ccb2e574c3b20af4
Detilly <bvjulien@gmail.com>
2021-08-26 10:33:32 -0400

    new filesystem

--- path.js
+++ path.js
@@ -1,2 +1,14 @@
 const path = require("path");
 console.log("Affichage de path.basename")
+console.log("Utilisation de path.basename", path.basename(__filename));
+console.log("utilisation", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
+console.log("", path.());
