b3c1b3479763849c4140f138951cb943efddffab
Detilly <bvjulien@gmail.com>
2021-08-26 10:44:08 -0400

    first commit

--- filesystem.js
+++ filesystem.js
@@ -1,2 +1,5 @@
 const fs = require("fs");
 const path = require("path");
+console.log("creation du dossier toto");
+fs.mkdir(path.join(__dirname, "toto"));
+console.log("le dossier toto a été créé");
