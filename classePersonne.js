541dc281bc6cfabcfbc3528afeaaadee0081a229
Detilly <bvjulien@gmail.com>
2021-08-26 09:22:06 -0400

    classePersonne

---
+++ classePersonne.js
@@ -0,0 +1,18 @@
+class Personne {
+
+    constructor(nom, age) {
+
+        this.nom = nom;
+        this.age = age;
+
+    }
+
+    salutations() {
+
+        console.log("Mon nom est &{this.nom} et j'ai &{this.age} ans")
+
+    }
+
+}
+
+module.exports = Personne;
